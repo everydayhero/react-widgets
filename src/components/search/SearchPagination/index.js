12e8838a3ef2ff4973b3ce6a4d9d04440c2f5696
Brendan Brewster <brendanb@everydayhero.com.au>
2014-10-14 10:11:47 +1000

    Fixed pagination from/to values

--- src/components/search/SearchPagination/index.js
+++ src/components/search/SearchPagination/index.js
@@ -10,24 +10,19 @@ module.exports = React.createClass({
     this.props.onChange(this.props.page + 1);
   },
   render: function () {
-    var count = this.props.count;
-
     if (this.props.totalPages <= 1) { return false; }
 
-    var from = (this.props.page - 1) * this.props.pageSize;
-    var to = from + this.props.pageSize;
+    var from = (this.props.page - 1) * this.props.pageSize + 1;
+    var to = Math.min(from + this.props.pageSize - 1, this.props.count);
 
     var isFirstPage = this.props.page <= 1;
     var isLastPage = this.props.page >= this.props.totalPages;
 
-    from = from ? from : 1;
-    to = (to > count) ? count : to;
-
     return (
       <div className='SearchPagination'>
         <div className="SearchPagination__counter">
-          <span className="SearchPagination__current">{from} - {to} </span>
-          <span className="SearchPagination__count">of {this.props.count}</span>
+          <span className="SearchPagination__current">{ from } - { to } </span>
+          <span className="SearchPagination__count">of { this.props.count }</span>
         </div>
         <button className='SearchPagination__button--left' onClick={ this.onPageLeft } disabled={ isFirstPage }>
           <Icon icon="chevron-left"/>

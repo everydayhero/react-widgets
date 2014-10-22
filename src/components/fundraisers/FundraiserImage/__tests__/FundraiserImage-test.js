b5eea9ee59ee3b13717d054c5f1095720bb128ec
Sam Enoka <slwenoka@gmail.com>
2014-10-22 13:34:23 +1000

    Set up basic test file for FundraiserImage.

---
+++ src/components/fundraisers/FundraiserImage/__tests__/FundraiserImage-test.js
@@ -0,0 +1,26 @@
+/** @jsx React.DOM */
+"use strict";
+jest.autoMockOff();
+
+describe('FundraiserImage', function() {
+  var React                       = require('react/addons');
+  var FundraiserImage             = require('../');
+  var TestUtils                   = React.addons.TestUtils;
+  var scryByClass                 = TestUtils.scryRenderedDOMComponentsWithClass;
+  var scryByTag                   = TestUtils.scryRenderedDOMComponentsWithTag;
+  var findByClass                 = TestUtils.findRenderedDOMComponentWithClass;
+
+  describe('component defaults', function() {
+    var fundraiserImage;
+    var element;
+
+    beforeEach(function() {
+      fundraiserImage = <FundraiserImage campaignUid="au-0" />;
+      element = TestUtils.renderIntoDocument(fundraiserImage);
+    });
+
+    it('renders something', function() {
+      expect(element).not.toBeNull();
+    });
+  });
+});

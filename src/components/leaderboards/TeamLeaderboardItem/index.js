bd5a38f50c996bc176ccfae27f4679c7ae801cb8
Sam Enoka <slwenoka@gmail.com>
2014-10-28 13:55:38 +1000

    Make leaderboard responsive.

--- src/components/leaderboards/TeamLeaderboardItem/index.js
+++ src/components/leaderboards/TeamLeaderboardItem/index.js
@@ -9,25 +9,27 @@ module.exports = React.createClass({
   render: function() {
     return (
       <li className="TeamLeaderboard__items-item">
-        <div className="TeamLeaderboard__items-image">
-          <img src={ this.props.imgSrc } />
-        </div>
-        <div className="TeamLeaderboard__items-content">
-          <div className="TeamLeaderboard__items-name">{ this.props.name }</div>
-          <div className="TeamLeaderboard__items-stats">
-            <div className="TeamLeaderboard__items-stat">
-              <div className="TeamLeaderboard__items-stat-content">
-                { this.props.totalMembers }
-                <div className="TeamLeaderboard__items-stat-title">
-                  { this.props.membersTitle }
+        <div className="TeamLeaderboard__items-skin">
+          <div className="TeamLeaderboard__items-image">
+            <img src={ this.props.imgSrc } />
+          </div>
+          <div className="TeamLeaderboard__items-content">
+            <div className="TeamLeaderboard__items-name">{ this.props.name }</div>
+            <div className="TeamLeaderboard__items-stats">
+              <div className="TeamLeaderboard__items-stat">
+                <div className="TeamLeaderboard__items-stat-content">
+                  { this.props.totalMembers }
+                  <div className="TeamLeaderboard__items-stat-title">
+                    { this.props.membersTitle }
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="TeamLeaderboard__items-stat">
-              <div className="TeamLeaderboard__items-stat-content">
-                { this.props.amount }
-                <div className="TeamLeaderboard__items-stat-title">
-                  { this.props.raisedTitle }
+              <div className="TeamLeaderboard__items-stat">
+                <div className="TeamLeaderboard__items-stat-content">
+                  { this.props.amount }
+                  <div className="TeamLeaderboard__items-stat-title">
+                    { this.props.raisedTitle }
+                  </div>
                 </div>
               </div>
             </div>

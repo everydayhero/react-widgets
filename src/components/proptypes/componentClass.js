bab1bf6e54160fc23993b9317fc6fb434a6e1987
Brendan Brewster <brendanb@everydayhero.com.au>
2014-10-30 09:39:03 +1000

    Extract PropType componentClass

---
+++ src/components/proptypes/componentClass.js
@@ -0,0 +1,22 @@
+var React = require('react');
+
+function createComponentClassChecker() {
+  function checkType(isRequired, props, propName, componentName) {
+    if (!props[propName]) {
+      if (!isRequired) {
+        return;
+      }
+      return new Error('Required prop `' + propName + '` was not specified in `' + componentName + '`.');
+    }
+    if (!React.isValidClass(props[propName])) {
+      return new Error('Invalid prop `' + propName + '` supplied to `' + componentName + '`, expected React Component class.');
+    }
+  }
+
+  var chainedCheckType = checkType.bind(null, false);
+  chainedCheckType.isRequired = checkType.bind(null, true);
+
+  return chainedCheckType;
+}
+
+module.exports = createComponentClassChecker();

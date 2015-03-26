b2e4d848675415262753cacebc82b8f18dba6779
noweverywhere <marinus@noweverywhere.ca>
2015-03-26 15:04:25 +1000

    made countdown widget link more configureable

--- src/components/events/CountDown/__tests__/CountDown-test.js
+++ src/components/events/CountDown/__tests__/CountDown-test.js
@@ -11,10 +11,10 @@ describe('Event', function() {
   describe('component defaults', function() {
     var element;
     var days = 100;
-    var registerUrl = 'http://www.google.com/';
+    var linkUrl = 'http://www.google.com/';
 
     beforeEach(function() {
-      var countDown = <CountDown days={ days } registerUrl={ registerUrl } />;
+      var countDown = <CountDown days={ days } linkUrl={ linkUrl } />;
       element = TestUtils.renderIntoDocument(countDown);
     });
 
@@ -33,7 +33,28 @@ describe('Event', function() {
 
     it('renders anchor with register url', function() {
       var node = findByClass(element, 'CountDown__register').getDOMNode();
-      expect(node.href).toBe(registerUrl);
+      expect(node.href).toBe(linkUrl);
+    });
+
+    it('renders anchor with default text', function() {
+      var node = findByClass(element, 'CountDown__register').getDOMNode();
+      expect(node.textContent).toBe('Get Started');
+    });
+  });
+
+  describe('component configurable', function() {
+    var element;
+    var days = 100;
+    var linkUrl = 'http://www.google.com/';
+
+    beforeEach(function() {
+      var countDown = <CountDown days={ days } i18n={{ linkText:'Register' }} linkUrl={ linkUrl } />;
+      element = TestUtils.renderIntoDocument(countDown);
+    });
+
+    it('renders anchor with provided text', function() {
+      var node = findByClass(element, 'CountDown__register').getDOMNode();
+      expect(node.textContent).toBe('Register');
     });
   });
 });

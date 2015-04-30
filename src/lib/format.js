4b7e2dbda994c963353c282abe1ca5f2143070f5
Brendan Brewster <brendanb@everydayhero.com.au>
2015-04-30 12:11:15 +1000

    Add numeral formatting to string format

--- src/lib/format.js
+++ src/lib/format.js
@@ -1,13 +1,23 @@
 "use strict";
 
+var numeral = require('numeral');
+
 function format(template, data, strip) {
-  if (!data) {
+  if (!data && !strip) {
     return template;
   }
 
+  data = data || {};
+
   return template.replace(/{([^{}]+)}/g, function(a, b) {
-    var r = data[b];
-    return (r !== null && r !== undefined) ? r : strip ? '' : a;
+    var parts = b.split(':');
+    var format = parts[1];
+    var key = parts[0];
+    var value = data[key];
+    if (value == null) {
+      return strip ? '' : a;
+    }
+    return format ? numeral(value).format(format) : value;
   });
 }
 

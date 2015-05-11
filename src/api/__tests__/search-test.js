2af22db1c94cdb9a9698b9dd5c09c30cae1f2fa7
Joel Richards <joelr@everydayhero.com.au>
2015-05-11 15:14:59 +1000

    We want searching to be a bit laxer

--- src/api/__tests__/search-test.js
+++ src/api/__tests__/search-test.js
@@ -16,13 +16,13 @@ describe('search', function() {
 
   describe('aggregate', function() {
     it('searches for everything', function() {
-      var query = { searchTerm: 'bar', country: 'xy', page: 2, pageSize: 7, minimumScore: 0.1 };
+      var query = { searchTerm: 'bar', country: 'xy', page: 2, pageSize: 7, minimumScore: 0.05 };
       var callback = jest.genMockFunction();
       search.aggregate(query, callback);
 
       expect(getJSONP).lastCalledWith(
         'https://everydayhero.com/api/v2/search/aggregate.jsonp' +
-          '?q=bar&country_code=xy&page=2&page_size=7&minimum_score=0.1',
+          '?q=bar&country_code=xy&page=2&page_size=7&minimum_score=0.05',
         callback, {timeout: 10000}
       );
       expect(callback).toBeCalledWith(results);

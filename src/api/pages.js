4c3449096d24f538f0b2d722f4c6d444348dc304
Sam Enoka <slwenoka@gmail.com>
2015-05-21 14:22:54 +1000

    Add method for pulling pages scoped with multiple campaign uids.

--- src/api/pages.js
+++ src/api/pages.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var _ = require('lodash');
-var routes = require('./routes');
-var getJSONP = require('../lib/getJSONP');
+var _         = require('lodash');
+var routes    = require('./routes');
+var getJSONP  = require('../lib/getJSONP');
 var campaigns = require('./campaigns');
 
 module.exports = {
@@ -23,7 +23,34 @@ module.exports = {
       page: page,
       limit: limit
     }, options);
+
     return getJSONP(routes.get('pages', params), callback);
+  },
+
+  findByCampaigns: function(campaignUids, type, limit, page, callback, options) {
+    if (_.isEmpty(campaignUids)) {
+      _.defer(callback, { campaigns: [] });
+      return;
+    }
+
+    var pages = [];
+
+    var done = _.after(campaignUids.length, function() {
+      return callback({ pages: _.sample(pages, limit) });
+    });
+
+    var storePages = function(result) {
+      if ("pages" in result) {
+        _.forEach(result.pages, function(page) {
+          pages.push(page);
+        });
+      }
+      done();
+    };
+
+    _.forEach(campaignUids, function(campaignUid) {
+      this.findByCampaign(campaignUid, type, limit, page, storePages, options);
+    }, this);
   },
 
   search: function(params, callback) {

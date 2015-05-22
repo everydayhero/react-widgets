5b6852f1657d8597444f1463bc2f5514806d6464
Brendan Brewster <brendanb@everydayhero.com.au>
2015-05-22 15:18:40 +1000

    Move aggregate search filter tabs out of scroll area

--- src/components/search/AggregateSearchModal/index.js
+++ src/components/search/AggregateSearchModal/index.js
@@ -160,7 +160,7 @@ module.exports = React.createClass({
         currentPage: pagination.current_page
       });
 
-      if (this.refs.body && pagination.current_page == 1) {
+      if (this.refs.body && pagination.current_page === 1) {
         this.refs.body.getDOMNode().scrollTop = 0;
       }
     } else {
@@ -313,10 +313,10 @@ module.exports = React.createClass({
           <span className="AggregateSearchModal__title">{ this.t('title') }</span>
           { this.renderCloseButton() }
           { this.renderInput() }
+          { this.renderFilters() }
         </div>
         <div ref="body" className="AggregateSearchModal__body">
           <div className="AggregateSearchModal__content">
-            { this.renderFilters() }
             { this.renderResults() }
           </div>
         </div>

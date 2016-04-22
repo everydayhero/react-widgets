db82f677e2353f8b05560bf5328bd5cbb2f20a2c
noweverywhere <marinus@noweverywhere.ca>
2016-04-22 16:19:54 +1000

    Transpile javascript for deployment to NPM

---
+++ gulp_tasks/transpile.js
@@ -0,0 +1,36 @@
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var clean = require('gulp-clean');
+
+var destination = 'dist';
+
+gulp.task("transpile", ['build-transpile'], function() {
+  return gulp.src( destination + '/src', {read: false})
+    .pipe(clean());
+});
+
+gulp.task("build-transpile", ['transpile-js', 'transpile-scss', 'transpile-images'], function() {
+  return gulp.src(['package.json', destination + '/src/**/*.*']).pipe(gulp.dest(destination));
+});
+
+
+gulp.task("transpile-js", function () {
+  return gulp.src([
+      `src/**/*.js`,
+      `!src/**/*-test.js`,
+    ])
+    .pipe(babel())
+    .pipe(gulp.dest(destination));
+});
+
+gulp.task('transpile-scss', function() {
+  return gulp
+    .src(['src/**/*.{scss,sass}', 'common.scss', 'assets.scss'])
+    .pipe(gulp.dest(destination))
+});
+
+gulp.task('transpile-images', function() {
+  return gulp
+    .src('./images/*')
+    .pipe(gulp.dest(`${destination}/images`));
+});
--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var replace      = require('gulp-replace')
 var pkg          = require('./package')
 var request      = require('superagent')
 var fs           = require('fs')
+var babel = require("gulp-babel");
 
 // stylesheets
 var sass         = require('gulp-sass')
@@ -22,6 +23,9 @@ var uglify       = require('gulp-uglify')
 var eslint       = require('gulp-eslint')
 var buffer       = require('vinyl-buffer')
 var source       = require('vinyl-source-stream')
+
+//TASKS
+require('./gulp_tasks/transpile');
 
 // html
 var inject       = require('gulp-inject')

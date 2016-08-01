975e649a540a8db4c281f059e7e61cc58f77b63b
leonf <leonfu103@gmail.com>
2016-08-01 14:39:53 +1000

    Add bin/specs and stuff related

--- test/helpers/common.js
+++ test/helpers/common.js
@@ -1,8 +1,10 @@
 'use strict'
 
-global.chai = require('chai')
-global.expect = chai.expect
-global.should = chai.should()
-global.sinon = require('sinon')
-global.assert = sinon.assert
-global.mockrequire = require('mockrequire')
+global.sinon = require('sinon');
+global.chai = require('chai');
+global.expect = chai.expect;
+global.should = chai.should();
+global.assert = chai.assert;
+global.mockrequire = require('mockrequire');
+
+chai.use(require('sinon-chai'));

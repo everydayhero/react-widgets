f18429708f6080fe82ea05aa44971ffdd98c0acf
Ellis Clayton <ellis@ellisclayton.info>
2016-10-10 15:09:31 +1000

    Add tests
      - Separate js DOM into common helper
      - Remove old js DOM from specs previously using it
      - Add new spec for Fitness Leaderboards

--- test/helpers/common.js
+++ test/helpers/common.js
@@ -1,5 +1,9 @@
 'use strict'
 
+require('./testdom')('<html><body></body></html>', {
+  url: 'http://localhost:8000'
+})
+
 global.sinon = require('sinon');
 global.chai = require('chai');
 global.expect = chai.expect;
---
+++ test/helpers/testdom.js
@@ -0,0 +1,13 @@
+'use strict';
+// Via http://www.asbjornenge.com/wwc/testing_react_components.html
+module.exports = (markup, options) => {
+  if (typeof document !== 'undefined') return;
+  var jsdom = require('jsdom').jsdom;
+  global.document = jsdom(markup || '', options);
+  global.window = document.defaultView;
+  global.navigator = {
+    userAgent: 'node.js'
+  };
+  // ... add whatever browser globals your tests might need ...
+}
+
